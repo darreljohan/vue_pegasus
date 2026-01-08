d0711e14e6e5179d336fe6775bd62d6d06b5d051
darreljohan <darreljohan162@gmail.com>
2026-01-08 22:11:57 +0700

    Add Schedule Page into default Router Page

--- src/router.js
+++ src/router.js
@@ -24,6 +24,7 @@ const router = createRouter({
                 {
                     component: SchedulePage,
                     path: '/schedule',
+                    alias: '/',
                     children:[
                         {
                             component: ScheduleUpsertForm,
